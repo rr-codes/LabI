68d4ac2307cc80ec9405a35256b8b57adff6eab7
Richard Robinson <rir@my.yorku.ca>
2021-03-11 22:03:28 -0500

    Create partB.ino

---
+++ partB.ino
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+
+void setup() {
+    Serial.begin(9600);
+}
+
+void sendPotentiometerData() {
+    const auto value = analogRead(A0);
+    const byte data[] = {0, 0, highByte(value), lowByte(value)};
+
+    Serial.write(data, 4);
+    Serial.println();
+}
+
+void loop() {
+    sendPotentiometerData();
+    delay(1000);
+}
